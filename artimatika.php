c980b473c79265a6351beaa8dc625ee0de06f844
ghinahanifah <71633122+ghinahanifah@users.noreply.github.com>
2020-10-11 19:39:53 +0800

    Add files via upload

---
+++ artimatika.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>Aritmatika</title>
+    </head>
+    <body>
+        <form id="form_text" name="form_text" method="POST" action="hasil_aritmatika.php">
+            <h1>inputkan suku baris aritmatika</h1>
+            <table widht="360px" cellpading="0" cellspacing="0">
+                <tr>
+                    <td>Suku Ke-1</td>
+                    <td>: <input type="text" id="1" name="1"></td>
+                </tr>
+                <tr>
+                    <td>Suku Ke-2</td>
+                    <td>: <input type="text" id="2" name="2"></td>
+                </tr>
+                <tr>
+                <tr>
+                    <td>Banyak Suku Ke-n</td>
+                    <td>: <input type="text" id="3" name="3"></td>
+                </tr>
+                <tr>
+                    <td><button type="submit" id="proses" name="proses">Proses</button></td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
